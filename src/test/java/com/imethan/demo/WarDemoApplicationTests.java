9507c446b500a4718ab7f0e175cdd6e7885b9d08
root <12345678>
2020-11-02 17:48:40 +0800

    modify class

---
+++ src/test/java/com/imethan/demo/WarDemoApplicationTests.java
@@ -0,0 +1,21 @@
+package com.imethan.demo;
+/**
+ * @Description: 
+ * @Author: Ethan Wong
+ * @Date: 2020-11-02 17:00:49
+ * @FilePath: \src\test\java\com\imethan\demo\SpringbootDemoWarApplicationTests.java
+ * @LastEditTime: 2020-11-02 17:47:46
+ * @LastEditors: your name
+ */
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
