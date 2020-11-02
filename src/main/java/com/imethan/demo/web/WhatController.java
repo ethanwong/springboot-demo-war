a99d18e956390181fc9267bdcb4e330424da1e93
root <12345678>
2020-11-02 17:44:02 +0800

    modify pom.xml

--- src/main/java/com/imethan/demo/web/WhatController.java
+++ src/main/java/com/imethan/demo/web/WhatController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description: 
  * @Author: Ethan Wong
  * @Date: 2020-10-31 15:01:29
- * @FilePath: -war\src\main\java\com\imethan\springbootdemowar\WhatController.java
+ * @FilePath: \src\main\java\com\imethan\demo\web\WhatController.java
  */
 @RestController
 public class WhatController {
